720b7a3fc56ef7173cd8687f0394e7952b1fac34
ippei-takahashi <aimluck.ippei.takahashi@gmail.com>
2013-04-24 11:46:29 +0900

    index作成

---
+++ simplechat_server/src/sample/chat/controller/IndexController.java
@@ -0,0 +1,11 @@
+package sample.chat.controller;
+
+import org.slim3.controller.Controller;
+import org.slim3.controller.Navigation;
+
+public class IndexController extends Controller {
+    @Override
+    public Navigation run() throws Exception {
+        return redirect("/login");
+    }
+}
